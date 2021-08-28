76f47f4f9507e05ef669b0fcc731116e4ee9490b
Tinchoxxo <87064404+Tinchoxxo@users.noreply.github.com>
2021-08-28 00:03:37 -0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'Expossed-BOT',
+        ownerName: 'Tincho',
+        youtube: 'https://www.youtube.com/channel/UC89XOPdzAtLuUPTUOEmXTdw',
+        instagram: 'https://www.instagram.com/tincho_maldonadoo/',
+}
